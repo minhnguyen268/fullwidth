e0134312cca362e341fa599e70a6250c44ab445b
minhnguyen268 <165470790+minhnguyen268@users.noreply.github.com>
2025-08-19 18:26:36 +0000

    Update withdraw.config.js

--- front-end/configs/withdraw.config.js
+++ front-end/configs/withdraw.config.js
@@ -1,6 +1,6 @@
 export const ADMIN_LIST_WITHDRAW_HISTORY_PAGE_SIZE = 10;
 
-export const MIN_MONEY_WITHDRAW = 400;
+export const MIN_MONEY_WITHDRAW = 20;
 
 export const TINH_TRANG_WITHDRAW_HISTORY = {
   DANG_CHO: "dangCho",
